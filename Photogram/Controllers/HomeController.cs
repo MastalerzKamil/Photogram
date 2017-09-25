4ed3193435b82f0bf5fff8409fef2fbd0e52192d
DESKTOP-T8ITJGP\kamil <kamil.mastalerz7@wp.pl>
2017-09-25 14:27:34 +0200

    Changed HomeView

--- Photogram/Controllers/HomeController.cs
+++ Photogram/Controllers/HomeController.cs
@@ -20,6 +20,13 @@ namespace Photogram.Controllers
             return View();
         }
 
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
