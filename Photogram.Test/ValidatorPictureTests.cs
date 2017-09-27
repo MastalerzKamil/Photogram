8661e1eba6e7d641189510f748b54618ae8b8796
DESKTOP-T8ITJGP\kamil <kamil.mastalerz7@wp.pl>
2017-09-27 20:46:29 +0200

    Added unit tests

--- Photogram.Test/ValidatorPictureTests.cs
+++ Photogram.Test/ValidatorPictureTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Photogram.Models.PictureViewModels;
 
 namespace Photogram.Test
 {
@@ -7,8 +9,50 @@ namespace Photogram.Test
     public class ValidatorPictureTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void isJPGMime_GoodMime_ReturnsImageJpeg()
         {
+            string path = "~/images/uploadedPicture/coffe.jpg";
+            ValidatorPicture uploadedPicture = new ValidatorPicture(path);
+
+            //ACT
+            string mime=uploadedPicture.extractMimePicture();
+
+            //ASSERT
+            Assert.AreEqual("image/jpeg", mime.ToString());
+        }
+
+        [TestMethod]
+        public void isBadPngMime_GivenPngWhenJpeg()
+        {
+            string path = "~/images/uploadedPicture/coffe.png";
+            ValidatorPicture uploadedPicture = new ValidatorPicture(path);
+
+            string mime = uploadedPicture.extractMimePicture();
+
+            Assert.AreNotEqual("image/png", mime.ToString());
+        }
+
+        [TestMethod]
+        public void isGoodPngMime_GivenPngWhenJpeg()
+        {
+            string path = "~/images/uploadedPicture/coffe.png";
+            ValidatorPicture uploadedPicture = new ValidatorPicture(path);
+
+            string mime = uploadedPicture.extractMimePicture();
+
+            Assert.AreEqual("image/png", mime.ToString());
+        }
+
+        [TestMethod]
+        public void extractingFileName_ReturnsFileName()
+        {
+            string path = "~/images/uploadedPicture/cityPhoto.jpg";
+            string searchedFileName = "cityPhoto.jpg";
+            ValidatorPicture uploadedFileName =new ValidatorPicture(path);
+
+            string fileName = uploadedFileName.PictureName;
+
+            Assert.AreEqual("cityPhoto.jpg", fileName.ToString());
         }
     }
 }
--- Photogram/Models/PictureViewModels/ValidatorPicture.cs
+++ Photogram/Models/PictureViewModels/ValidatorPicture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Runtime.InteropServices.ComTypes;
@@ -12,17 +13,28 @@ namespace Photogram.Models.PictureViewModels
     {
         public string PicturePath { get; set; }
         public string PictureName { get; set; }
+        public string MimeType { get; set; }
 
         public ValidatorPicture(string path)
         {
             this.PicturePath = path;
+            this.PictureName = Path.GetFileName(this.PicturePath);
+            this.MimeType = extractMimePicture();
         }
 
-        public string isPictureJPG()
+        public string extractMimePicture()
         {
             string mime = MimeMapping.MimeUtility.GetMimeMapping(PicturePath);
             return mime;
-            return null;
+        }
+
+        public bool validatePicture()
+        {
+            if (this.MimeType == "iamge/jpeg" || this.MimeType == "image/png")
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
